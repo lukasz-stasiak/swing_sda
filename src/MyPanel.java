2edecdb01dce689b66257a0682ff0db44944d0a3
Lukasz <uzytkownik@kursGIT.pl>
2021-02-28 21:59:19 +0100

    Rozmieszczenie

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,61 +1,61 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class MyPanel extends JPanel{// implements ActionListener{
-    JList<String> lista;
-    DefaultListModel<String> model; // tutaj przygotowuje i pozniej dodaje
-    JTextField input;
+class MyPanel extends JPanel implements ActionListener {
+    JButton button, buttonScore;
+    JTextField textField, textField2;
+    JLabel label, label2, label3;
 
-//    class Akcja implements ActionListener{
-//
-//        @Override
-//        public void actionPerformed(ActionEvent e) {
-//            model.addElement(input.getText());
-//            input.setText("");
-//        }
-//    }
 
     public MyPanel() {
         setLayout(null);
-        model = new DefaultListModel<String>();
-        lista = new JList<String>();
-        lista.setModel(model);
+        
 
-        JScrollPane scrollPane = new JScrollPane(lista);
-        scrollPane.setBounds(10, 10, 480, 350);
-        add(scrollPane);
+        label2 = new JLabel("Jest to gra do zgadywania liczb. Będziemy losować liczbę od 1 do 200.");
+label2.setBounds(10, 5, 600, 30);
+add(label2);
 
-        input = new JTextField();
-        input.setBounds(180, 410, 150, 20);
-        add(input);
+        label3 = new JLabel("Spróbuj odgadnąć w najmniejszej liczbię kroków co to za liczba");
+        label3.setBounds(10, 20, 600, 30);
+        add(label3);
+
+        textField = new JTextField("Podaj liczbę ");
+        textField.setBounds(10, 60, 200, 20);
+        add(textField);
+
+        label = new JLabel("test");
+        label.setBounds(10, 140, 600, 20);
+        add(label);
+
+        button = new JButton("Sprawdź!");
+        button.setBounds(10, 90, 150, 30);
+        add(button);
+        button.addActionListener(this);
+
+        buttonScore = new JButton("Pokaż tabelę wyników");
+        buttonScore.setBounds(10, 200, 200, 30);
+        add(buttonScore);
+        buttonScore.addActionListener(this);
 
 
-        JButton klawisz = new JButton("Kliknij mnie!");
-        klawisz.setBounds(180, 440, 150, 20);
-//        klawisz.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                model.addElement(input.getText());
-//                input.setText("");
-//            }
-//        });
 
-        klawisz.addActionListener(e -> { // wyrazenie lambda
-            model.addElement(input.getText());
-            input.setText("");
-        });
-        add(klawisz);
     }
-
 
     @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(500, 500);
+    public void actionPerformed(ActionEvent e) {
+       // JOptionPane.showMessageDialog(null, "Hello");
+label.setText("hello");
+
     }
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        model.addElement(input.getText());
-//        input.setText("");
-//    }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(500, 400);
+    }
+
+
+
+
 }
+
