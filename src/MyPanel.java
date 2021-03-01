227a0b1491e78535ebdeb92b864fffa492d15423
Lukasz <uzytkownik@kursGIT.pl>
2021-03-01 10:56:32 +0100

    PopUp window

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,49 +1,75 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.Random;
 
 
 class MyPanel extends JPanel implements ActionListener {
     JButton button, buttonScore;
-    JTextField textField, textField2;
+    JTextField textField, textField2, textValid;
     JLabel label, label2, label3, label4;
+    JTextArea textArea;
 
 
     int hit;
     int iloscTrafien = 1;
 
-    Random r = new Random();
-    int x = r.nextInt(201);
+  /*  Random r = new Random();
+    int x = r.nextInt(201);*/
 
-  /*  int x = 2;*/
+    int x = 2;
 
 
     public MyPanel() {
         setLayout(null);
-        
+setBackground(Color.CYAN);
 
         label2 = new JLabel("Jest to gra do zgadywania liczb. Będziemy losować liczbę od 1 do 200.");
         label2.setBounds(10, 5, 600, 30);
+        label2.setFont(new Font("Arial", Font.BOLD, 15));
         add(label2);
 
         label3 = new JLabel("Spróbuj odgadnąć w najmniejszej liczbię kroków co to za liczba");
-        label3.setBounds(10, 20, 600, 30);
+        label3.setBounds(10, 24, 600, 30);
+        label3.setFont(new Font("Arial", Font.BOLD, 15));
         add(label3);
 
 
-        label4 = new JLabel("Podaj liczbę od 1 do 200");
-        label4.setBounds(10, 60, 600, 30);
+        label4 = new JLabel("Podaj liczbę od 1 do 200:");
+        label4.setBounds(10, 70, 600, 30);
+        label4.setFont(new Font("Arial", Font.BOLD, 14));
         add(label4);
 
+        label= new JLabel();
+        label.setBounds(250, 90, 200, 30);
+        add(label);
+
         textField = new JTextField();
-        textField.setBounds(170, 70, 200, 20);
+        textField.setBounds(190, 70, 200, 30);
+        textField.setFont(new Font("Arial", Font.BOLD, 15));
         add(textField);
 
-        label = new JLabel("Tutaj pokaże się wynik");
-        label.setBounds(10, 140, 600, 20);
-        add(label);
+   /*     textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                try {
+                    int in = Integer.parseInt(textField.getText());
+                    label.setText("");
+                }catch (NumberFormatException e1) {
+                    label.setText("Podaj wartość liczbową");
+
+                }
+            }
+        });*/
+
+
+
+        textArea = new JTextArea("Tutaj pokaże się wynik");
+        textArea.setBounds(10, 150, 600, 50);
+        textArea.setFont(new Font("Arial", Font.BOLD, 15));
+        textArea.setLineWrap(true);
+        textArea.setBackground(null);
+        add(textArea);
 
         button = new JButton("Sprawdź!");
         button.setBounds(60, 110, 150, 30);
@@ -51,7 +77,7 @@ class MyPanel extends JPanel implements ActionListener {
         button.addActionListener(this);
 
         buttonScore = new JButton("Pokaż tabelę wyników");
-        buttonScore.setBounds(10, 200, 200, 30);
+        buttonScore.setBounds(10, 220, 200, 30);
         add(buttonScore);
         buttonScore.addActionListener(this);
 
@@ -65,16 +91,35 @@ class MyPanel extends JPanel implements ActionListener {
 int liczba = Integer.parseInt(textField.getText());
 
         if (liczba == x) {
-            label.setText("ZGADŁEŚ GRATULACJE!! trafienie nastąpiło po: " + iloscTrafien + " strzałów");
+            textArea.setText("BRAWO!! Trafiłeś po: " + iloscTrafien + " strzałach");
+            setBackground(Color.YELLOW);
+            iloscTrafien=1;
+            textField.setText("");
+
+            //MESSAGE BOX
+            int response = JOptionPane.showConfirmDialog(null,
+                    "Zapisać wynik?", "Wybierz opcje...",JOptionPane.YES_NO_OPTION);
+            if (response ==  JOptionPane.YES_NO_OPTION) {
+                System.out.println("Zapisano wynik");
+                setBackground(Color.CYAN);
+            } else if (response == JOptionPane.NO_OPTION) {
+                System.out.println("Nie zapisano wyniku");
+                setBackground(Color.CYAN);
+            } else if (response == JOptionPane.CLOSED_OPTION) {
+                System.out.println("Nie zapisano wyniku. CANCEL");
+                setBackground(Color.CYAN);
+            }
+       /*     // 0=yes, 1=no, 2=cancel
+            System.out.println(response);*/
 
 
         } else if (liczba > x) {
-            label.setText("Niestety strzał chybiony, poszukiwana liczba jest MNIEJSZA od"  + liczba + ". Podaj kolejny strzał");
+            textArea.setText("Niestety strzał chybiony, poszukiwana liczba jest MNIEJSZA od "  + liczba + ".\n Podaj kolejny strzał");
 
             iloscTrafien++;
         } else {
 
-            label.setText("Niestety strzał chybiony, poszukiwana liczba jest WIĘKSZA od " + liczba + ".  Podaj kolejny strzał");
+            textArea.setText("Niestety strzał chybiony, poszukiwana liczba jest WIĘKSZA od " + liczba + ".\n Podaj kolejny strzał");
             iloscTrafien++;
         }
 
